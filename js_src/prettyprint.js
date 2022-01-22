c6254252e417962b7ecd1d9a63425a39fa46d9e7
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2022-01-22 13:06:18 +0300

    pretty printer for AST

--- js_src/prettyprint.js
+++ js_src/prettyprint.js
@@ -1,7 +1,11 @@
 import _ from "lodash";
 
+
+
 const symbolRe = /^[a-z][a-zA-Z0-9_]*$/
 const doesSymbolNeedEscaping = (str) => !symbolRe.test(str);
+
+
 
 const ppStr = (value) => `"${value['string']}"`;
 const ppInt = (value) => String(value);
@@ -12,22 +16,18 @@ const ppSymbol = (value) => {
   return value['symbol'];
 }
 const ppVar = ({ name, aggFunc, locPrefix }) => {
-  if (aggFunc['symbol'] != 'none') {
-    if (locPrefix['symbol'] == 'true') {
+  if (aggFunc && aggFunc['symbol'] != 'none') {
+    if (locPrefix && locPrefix['symbol'] == 'true') {
       throw new Error('Location prefix and aggregation at the same time');
     }
     return `${aggFunc['symbol']}<${name['symbol']}>`;
   }
 
-  if (locPrefix['symbol'] == 'true') {
+  if (locPrefix && locPrefix['symbol'] == 'true') {
     return `#${name['symbol']}`;
   }
 
-  if (locPrefix['symbol'] == 'false') {
-    return `${name['symbol']}`;
-  }
-
-  throw new Error('Incorrect var for pretty printing')
+  return `${name['symbol']}`;
 };
 
 const prettyPrintValue = (value) => {
@@ -71,23 +71,22 @@ const prettyPrintFacts = (tFacts) => {
 
 
 const atomsAndClausesByFileAndLineNum = (astFacts) => {
-  const atomsFilenames = astFacts
-    .get('ast_atom_location/3').map(t => t[0]);
-  const clausesFilenames = astFacts
-    .get('ast_clause_location/3').map(t => t[0]);
+  const atomsFilenames = (astFacts.get('ast_atom_location/3') ?? [])
+    .map(t => t[0]);
+  const clausesFilenames = (astFacts.get('ast_clause_location/3') ?? [])
+    .map(t => t[0]);
   const filenames = _.uniqBy(
     [...atomsFilenames, ...clausesFilenames],
     _.isEqual);
   
   return (new Map(filenames.map(filename => {
-    const atomsPairs = astFacts.get('ast_atom_location/3')
+    const atomsPairs = (astFacts.get('ast_atom_location/3') ?? [])
       .filter(t => _.isEqual(t[0], filename))
       .map(([_, line, atomId]) => ({ atomId, line }));
-    const clausesPairs = astFacts.get('ast_clause_location/3')
+    const clausesPairs = (astFacts.get('ast_clause_location/3') ?? [])
       .filter(t => _.isEqual(t[0], filename))
       .map(([_, line, clauseId]) => ({ clauseId, line }));
-    const sortedIds = _.sortBy([...atomsPairs, ...clausesPairs], t => t[0])
-      .map(({ line }) => line);
+    const sortedIds = _.sortBy([...atomsPairs, ...clausesPairs], t => t[0]);
     return [filename, sortedIds];
   })));
 };
@@ -98,26 +97,19 @@ const prettyPrintAtom = (astFacts, atomId) => {
   const [name, _id, timestamp] = _.find(
     astFacts.get('ast_atom/3'),
     ([_name, id, _timestamp]) => _.isEqual(id, atomId));
-  
-  const symArgsPairs = astFacts.get('ast_atom_sym_arg/3')
-    .filter(([id, index, value]) => _.isEqual(id, atomId))
-    .map(([index, value]) => [index, ppSymbol(value)]);
-  const intArgsPairs = astFacts.get('ast_atom_int_arg/3')
-    .filter(([id, index, value]) => _.isEqual(id, atomId))
-    .map(([index, value]) => [index, ppInt(value)]);
-  const strArgsPairs = astFacts.get('ast_atom_str_arg/3')
-    .filter(([id, index, value]) => _.isEqual(id, atomId))
-    .map(([index, value]) => [index, ppInt(value)]);
 
-  const argsStrs = _.sortBy(
-    [...symArgsPairs, ...intArgsPairs, ...strArgsPairs],
-    ([index, str]) => index);
-  
-  const expectedIndexes = _.range(1, argsStrs.length+1);
-  const [collectedIndexes, _strs] = _.unzip(argsStrs);
-  if (!_.isEqual(expectedIndexes, collectedIndexes)) {
-    throw new Error(`Incorrect indexes for atom: ${ppSymbol(atomId)}`);
-  }
+  const keep = ([id, index, value]) => _.isEqual(id, atomId);
+  const argsStrs = collectArguments(astFacts, {
+    'ast_atom_sym_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppSymbol(value)]
+    },
+    'ast_clause_int_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppInt(value)]
+    },
+    'ast_clause_str_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppStr(value)]
+    },
+  });
 
   if (argsStrs.length == 0) {
     return [`${ppSymbol(name)}@${ppInt(timestamp)};`];
@@ -126,46 +118,171 @@ const prettyPrintAtom = (astFacts, atomId) => {
   return [`${ppSymbol(name)}(${argsStrs.join(', ')})@${ppInt(timestamp)};`];
 };
 
-const prettyPrintBody = (astFacts, clauseId) => {
-  // add identation
-  //
-  // Currently order of body expressions is not stored in AST.
-  // let's print in following order:
-  //
-  //    positive, choose, negative, binop
 
-  return [];
+
+const collectArguments = (astFacts, selectors) => {
+  // selectors = { [key]: { keep, getPair }, ... }
+
+  const pairs = Object.keys(selectors).flatMap(key => {
+    const { keep, getPair } = selectors[key];
+    return (astFacts.get(key) ?? []).filter(keep).map(getPair);
+  });
+
+  const sortedPairs = _.sortBy(pairs, ([index, _val]) => index);
+
+  if (sortedPairs.length === 0) {
+    return [];
+  }
+
+  const expectedIndexes = _.range(1, sortedPairs.length+1);
+  const [collectedIndexes, values] = _.unzip(sortedPairs);
+  if (!_.isEqual(expectedIndexes, collectedIndexes)) {
+    throw new Error(`Incorrect args indexes: ${JSON.stringify(collectedIndexes)}`);
+  }
+
+  return values;
+}
+
+
+
+const prettyPrintBodyExpr = (astFacts, exprId, isLastExpr) => {
+  const indent = "  ";
+
+  const keep = ([id, index, name]) => _.isEqual(id, exprId);
+  const argsStrs = collectArguments(astFacts, {
+    'ast_body_sym_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppSymbol(value)]
+    },
+    'ast_body_int_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppInt(value)]
+    },
+    'ast_body_str_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppStr(value)]
+    },
+    'ast_body_var_arg/4': {
+      keep, getPair: ([id, index, name, locPrefix]) =>
+      [index, ppVar({ name, locPrefix })]
+    },
+  });
+
+  const chooseKeyVars = collectArguments(astFacts, {
+    'ast_body_choose_key_var/3': {
+      keep, getPair: ([id, index, name]) => [index, ppVar({ name })]
+    },
+  });
+  const chooseRowVars = collectArguments(astFacts, {
+    'ast_body_choose_row_var/3': {
+      keep, getPair: ([id, index, name]) => [index, ppVar({ name })]
+    },
+  });
+
+  const binop = _.find(
+    astFacts.get('ast_body_binop/2'),
+    ([id, op]) => _.isEqual(id, exprId));
+  const atom = _.find(
+    astFacts.get('ast_body_atom/3'),
+    ([id, name, negated]) => _.isEqual(id, exprId));
+  const choose = _.find(
+    astFacts.get('ast_body_choose/1'),
+    ([id]) => _.isEqual(id, exprId));
+
+  const numPresent = _.sum([binop, atom, choose].map(e => !!e ? 1 : 0));
+  if (numPresent !== 1) {
+    debugger
+    throw new Error(`Incorrect AST, several exprs for same id ${ppSymbol(exprId)}`);
+  }
+
+  let result = indent;
+
+  if (binop) {
+    const [_id, op] = binop;
+    if (argsStrs.length != 2) {
+      throw new Error('Exactly two arguments for binary operation expected');
+    }
+    result += `${argsStrs[0]} ${op['symbol']} ${argsStrs[1]}`;
+
+  } else if (atom) {
+    const [ id, name, negated ] = atom;
+    if (negated['symbol'] === 'true') {
+      result += `notin ${ppSymbol(name)}`;
+    } else {
+      result += ppSymbol(name);
+    }
+    if (argsStrs.length != 0) {
+      result += `(${argsStrs.join(', ')})`;
+    }
+
+    const intTimestamp = _.find(
+      astFacts.get('ast_body_atom_int_time/2'),
+      ([id, value]) => _.isEqual(id, exprId));
+    const varTimestamp = _.find(
+      astFacts.get('ast_body_atom_var_time/2'),
+      ([id, value]) => _.isEqual(id, exprId));
+    
+    if (intTimestamp) {
+      const [id, val] = intTimestamp;
+      result += `@${ppInt(val)}`;
+    } else if (varTimestamp) {
+      const [id, name] = varTimestamp ;
+      result += `@${ppVar({ name })}`;
+    }
+  } else if (choose) {
+    result += `choose((${chooseKeyVars.join(', ')}), (${chooseRowVars.join(', ')}))`;
+  } else {
+    throw new Error(`Unknown expr for id ${exprId}`);
+  }
+
+  if (isLastExpr) {
+    result += ';';
+  } else {
+    result += ','
+  }
+
+  return [result];
 };
+
+
+
+const prettyPrintBody = (astFacts, clauseId) => {
+  const exprIds = astFacts.get('ast_body_expr/2')
+    .filter(([clauseId1, exprId]) => _.isEqual(clauseId, clauseId1))
+    .map(([clauseId1, exprId]) => exprId);
+  const exprPairs = exprIds.map((exprId, index) => {
+    const [_fname, linenum, _id] = _.find(
+      astFacts.get('ast_body_expr_location/3'),
+      ([_fname, _line, exprId1]) => _.isEqual(exprId1, exprId));
+    const isLastExpr = (index == exprIds.length-1);
+    return [linenum, prettyPrintBodyExpr(astFacts, exprId, isLastExpr)];
+  });
+
+  const [_linenums, exprStrs] = _.unzip(_.sortBy(exprPairs, ([line, str]) => line));
+
+  return exprStrs;
+};
+
+
 
 const prettyPrintClause = (astFacts, clauseId) => {
   const [name, _id, suffix] = _.find(
     astFacts.get('ast_clause/3'),
     ([_name, id, _suffix]) => _.isEqual(id, clauseId));
   
-  const symArgsPairs = astFacts.get('ast_clause_sym_arg/3')
-    .filter(([id, index, value]) => _.isEqual(id, clauseId))
-    .map(([index, value]) => [index, ppSymbol(value)]);
-  const intArgsPairs = astFacts.get('ast_clause_int_arg/3')
-    .filter(([id, index, value]) => _.isEqual(id, clauseId))
-    .map(([index, value]) => [index, ppInt(value)]);
-  const strArgsPairs = astFacts.get('ast_clause_str_arg/3')
-    .filter(([id, index, value]) => _.isEqual(id, clauseId))
-    .map(([index, value]) => [index, ppInt(value)]);
-  const varArgsPairs = astFacts.get('ast_clause_var_arg/5')
-    .filter(([id, index, value]) => _.isEqual(id, clauseId))
-    .map(([index, name, aggFunc, locPrefix]) =>
-      [index, ppVar({ name, aggFunc, locPrefix })]);
-
-  const argsStrs = _.sortBy(
-    [ ...symArgsPairs, ...intArgsPairs,
-      ...strArgsPairs, ...varArgsPairs],
-    ([index, str]) => index);
-  
-  const expectedIndexes = _.range(1, argsStrs.length+1);
-  const [collectedIndexes, _strs] = _.unzip(argsStrs);
-  if (!_.isEqual(expectedIndexes, collectedIndexes)) {
-    throw new Error(`Incorrect indexes for atom: ${ppSymbol(atomId)}`);
-  }
+  const keep = ([id, _index, _value]) => _.isEqual(id, clauseId);
+  const argsStrs = collectArguments(astFacts, {
+    'ast_clause_sym_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppSymbol(value)]
+    },
+    'ast_clause_int_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppInt(value)]
+    },
+    'ast_clause_str_arg/3': {
+      keep, getPair: ([id, index, value]) => [index, ppStr(value)]
+    },
+    'ast_clause_var_arg/5': {
+      keep, getPair: ([id, index, name, aggFunc, locPrefix]) =>
+      [index, ppVar({ name, aggFunc, locPrefix })]
+    },
+  });
 
   let head = ppSymbol(name);
   if (argsStrs.length != 0) {
@@ -197,9 +314,9 @@ const prettyPrintAST = (astFacts) => {
   // order by filename.
 
   const orderedIds = atomsAndClausesByFileAndLineNum(astFacts);
-
-  const lines0 = orderedIds.keys().flatMap(filename => {
-    const lines = orderedIds.flatMap(arg => {
+debugger
+  const lines0 = [...orderedIds].flatMap(([filename, ids]) => {
+    const lines = ids.flatMap(arg => {
       const { atomId, clauseId } = arg;
       if (atomId) {
         return prettyPrintAtom(astFacts, atomId);
@@ -211,9 +328,12 @@ const prettyPrintAST = (astFacts) => {
       throw new Error(`Unknown arg for pretty printing: ${JSON.stringify(arg)}`);
     });
 
-    return [`%%% file: ${filename}`, ...lines];
+    return [`%%% file: ${filename['string']}`, ...lines];
   });
 
+  if (lines0.length === 0) {
+    return '';
+  }
   return lines0.reduce((acc, line) => acc.concat(`\n${line}`));
 }
 
